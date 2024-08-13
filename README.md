6c4e0bbb0c4eef79bc38e0823160c261a2327040
AndreiLucas123 <andrei.lucas.goncalves@gmail.com>
2024-08-13 12:08:31 -0300

    Fixed solid

--- README.md
+++ README.md
@@ -134,33 +134,44 @@ setSignalFactory(signal);
 Ou simplesmente copie o código do wrapper abaixo
 
 ```ts
-import { createEffect, createRoot, createSignal } from 'solid-js';
+import { createSignal } from 'solid-js';
 
 //
 setSignalFactory(signal);
 
 //
 export function signal<T>(initial: T): Signal<T> {
+  let _value = initial;
   const [value, setValue] = createSignal<T>(initial);
+
+  const callbacks = new Set<(value: T) => void>();
+
+  const subscribe = (callback: (value: T) => void) => {
+    callback(_value);
+    callbacks.add(callback);
+    return () => {
+      callbacks.delete(callback);
+    };
+  };
 
   return {
     get value() {
       return value();
     },
     set value(newValue) {
-      setValue(newValue as any);
+      _value = newValue;
+      if (typeof newValue === 'function') {
+        setValue(() => newValue);
+      } else {
+        setValue<any>(newValue);
+      }
+
+      for (const callback of callbacks) {
+        callback(newValue);
+      }
     },
 
-    subscribe(callback: (value: any) => void) {
-      let dispose: () => void;
-      createRoot((disposer) => {
-        dispose = disposer;
-        createEffect(() => callback(value()));
-      });
-
-      // @ts-ignore - The return type is correct.
-      return dispose;
-    },
+    subscribe,
   };
 }
 ```
--- tests/signal-factory.test.ts
+++ tests/signal-factory.test.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { setSignalFactory, signalFactory } from '../src';
 import { signal as vanillaSignal } from '../src/wrappers/vanilla';
+import { signal as vueSignal } from '../src/wrappers/vue';
+import { signal as solidSignal } from '../src/wrappers/solid';
 
 //
 //
@@ -58,4 +60,100 @@ test('Vanilla signal factory', () => {
   signal.value = 'unsubscribed';
 
   expect(values).toEqual(['hello', 'world']);
+});
+
+//
+//
+
+test('vue signal factory', async () => {
+  const signal = vueSignal('hello');
+
+  const values: string[] = [];
+
+  const unsubscribe = signal.subscribe((value) => {
+    values.push(value);
+  });
+
+  signal.value = 'world';
+
+  // Wait for the next tick.
+  await new Promise((resolve) => setTimeout(resolve, 0));
+
+  unsubscribe();
+
+  signal.value = 'unsubscribed';
+
+  expect(values).toEqual(['hello', 'world']);
+});
+
+//
+//
+
+test('vue signal factory should not call if value is a function', async () => {
+  const notCall1 = () => {
+    throw new Error('This should not be called');
+  };
+  const notCall2 = () => notCall1();
+
+  const signal = vueSignal(notCall1);
+
+  const values: (() => void)[] = [];
+
+  signal.subscribe((value) => {
+    values.push(value);
+  });
+
+  signal.value = notCall2;
+
+  // Wait for the next tick.
+  await new Promise((resolve) => setTimeout(resolve, 0));
+
+  expect(values).toEqual([notCall1, notCall2]);
+});
+
+//
+//
+
+test('solid signal factory', async () => {
+  const signal = solidSignal('hello');
+
+  const values: string[] = [];
+
+  const unsubscribe = signal.subscribe((value) => {
+    values.push(value);
+  });
+
+  signal.value = 'world';
+
+  unsubscribe();
+
+  signal.value = 'unsubscribed';
+
+  expect(values).toEqual(['hello', 'world']);
+});
+
+
+//
+//
+
+test('solid signal factory should not call if value is a function', async () => {
+  const notCall1 = () => {
+    throw new Error('This should not be called');
+  };
+  const notCall2 = () => notCall1();
+
+  const signal = solidSignal(notCall1);
+
+  const values: (() => void)[] = [];
+
+  signal.subscribe((value) => {
+    values.push(value);
+  });
+
+  signal.value = notCall2;
+
+  // Wait for the next tick.
+  await new Promise((resolve) => setTimeout(resolve, 0));
+
+  expect(values).toEqual([notCall1, notCall2]);
 });
