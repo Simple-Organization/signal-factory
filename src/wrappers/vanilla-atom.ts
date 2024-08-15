07a6d37d5c1a027853bd50732147c667bce5d3a4
AndreiLucas123 <andrei.lucas.goncalves@gmail.com>
2024-08-15 14:21:28 -0300

    Added selector to vanilla-atom

--- src/wrappers/vanilla-atom.ts
+++ src/wrappers/vanilla-atom.ts
@@ -28,3 +28,46 @@ export function atom<T>(initial: T): Signal<T> {
     subscribe,
   };
 }
+
+//
+//
+
+export function selector<E, T extends Readonly<[...Signal<E>[]]>, U>(
+  from: T,
+  getter: (values: E[]) => U,
+): Signal<U> {
+  const callbacks = new Set<(value: U) => void>();
+
+  //
+  //
+
+  let values = from.map((signal) => signal.value);
+  let value = getter(values);
+
+  //
+  //
+
+  const subscribe = (callback: (value: U) => void) => {
+    callback(value);
+    callbacks.add(callback);
+    return () => {
+      callbacks.delete(callback);
+    };
+  };
+
+  //
+  //
+
+  return {
+    get value() {
+      return value;
+    },
+    set value(newValue) {
+      value = newValue;
+      for (const callback of callbacks) {
+        callback(value);
+      }
+    },
+    subscribe,
+  };
+}
